642a5e051fe4afefe8ee3775ba0d5c5c3fdd97b5
MrNarman <godricknarman21@gmail.com>
2024-04-11 18:04:29 +0300

    Added a set of integers that are multiplied by each other

--- firstApp.java
+++ firstApp.java
@@ -1,5 +1,12 @@
 public class firstApp{
     public static void main(String[] args){
         System.out.println("Hello world");
+
+        int myNum = 34;
+        int otherNum = 20;
+
+        int result = myNum * otherNum;
+
+        System.out.println(result);
     }
 }
